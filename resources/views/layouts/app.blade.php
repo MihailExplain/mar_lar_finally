cc21223afd4c2510f7786f34a11ae0c91988601f
studentte1-1 <test@test.com>
2021-10-22 20:51:35 +0300

    view of default layouts

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Tasks</title>
+
+        <!-- CSS и JavaScript -->
+    </head>
+
+    <body>
+    <div class="container">
+        <nav class="navbar navbar-default">
+            <!-- Содержимое Navbar -->
+        </nav>
+    </div>
+        @yield('content')
+    </body>
+</html>
